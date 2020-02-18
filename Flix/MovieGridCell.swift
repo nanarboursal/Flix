cabebc853ea35c8cf94f914fceaa927ac2770b12
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-02-18 15:14:08 -0800

    added a get similar movies page

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Nanar Boursalian on 2/18/20.
+//  Copyright © 2020 NanarBoursalian. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
